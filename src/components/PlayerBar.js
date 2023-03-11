decbe9330713700b4486c86c1f910c1b23ce3415
khacdat328 <85425062+khacdat328@users.noreply.github.com>
2023-03-12 02:25:27 +0700

    Commit Left Sidebar #1

--- src/components/PlayerBar.js
+++
@@ -1,5 +0,0 @@
-function PlayerBar() {
-    return ( <div>Player Bar</div> );
-}
-
-export default PlayerBar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,19 @@
 import config from "~/config"
 
-import { Discovery, Following, NewSongs, Personal, ZingChart } from "~/page"
+import { Discovery, Following, NewSongs, Personal, ZingChart, Radio } from "~/page"
+import Category from "~/page/Category"
+import MV from "~/page/MV"
+import Top from "~/page/Top"
 const publicRoutes = [
 	{ path: config.routes.discovery, component: Discovery },
 	{ path: config.routes.following, component: Following },
 	{ path: config.routes.newsongs, component: NewSongs },
 	{ path: config.routes.personal, component: Personal },
 	{ path: config.routes.zingchart, component: ZingChart },
+	{ path: config.routes.radio, component: Radio },
+	{ path: config.routes.category, component: Category },
+	{ path: config.routes.mv, component: MV },
+	{ path: config.routes.top, component: Top },
 ]
 
 const privateRoutes = []
