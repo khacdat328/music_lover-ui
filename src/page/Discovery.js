b445d46e86240e908168a7a24d9df01969946891
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-03-12 17:00:49 +0700

    test listview

--- src/page/Discovery.js
+++ src/page/Discovery.js
@@ -1,7 +1,105 @@
+import ListView from "~/components/ListView";
+const songs = [
+	{
+		_id: "61bf8bdbbc1f883d7e782975",
+		songName: "Get Used To Me",
+		artistName: "Justin Bieber",
+		artistId: ["6162d9492fddb7174c15dc3f"],
+		albumId: "61bf95e3bc1f883d7e78297c",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 40,
+		songImg: "uploads/image00010.jpeg",
+		songSrc: "uploads/audio00013.mp3",
+		__v: 0,
+		create_at: "2021-11-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bf8c48bc1f883d7e782976",
+		songName: "Been You",
+		artistName: "Justin Bieber",
+		artistId: ["6162d9492fddb7174c15dc3f"],
+		albumId: "61bf95e3bc1f883d7e78297c",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 102,
+		songImg: "uploads/image00010.jpeg",
+		songSrc: "uploads/audio00014.mp3",
+		__v: 0,
+		create_at: "2021-11-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bf9c89d2b2d206fd98146c",
+		songName: "Lay It All On Me",
+		artistName: "Ed Sheeran",
+		artistId: ["6162d6f62fddb7174c15dc35"],
+		albumId: "6162e25f2d1ed5ed801ed306",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 175,
+		songImg: "uploads/image00002.jpeg",
+		songSrc: "uploads/audio00017.mp3",
+		__v: 0,
+		create_at: "2021-11-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bfd8e6d2b2d206fd981470",
+		songName: "Closer",
+		artistName: "The Chainsmokers, Halsey",
+		artistId: ["6162d7312fddb7174c15dc39"],
+		albumId: "6162e12d2d1ed5ed801ed304",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 103,
+		songImg: "uploads/image00001.jpeg",
+		songSrc: "uploads/audio00021.mp3",
+		__v: 0,
+		create_at: "2020-11-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bfdb07d2b2d206fd981475",
+		songName: "Maybe Idk",
+		artistName: "Jon Bellion",
+		artistId: ["6162d98c2fddb7174c15dc41"],
+		albumId: "61bf911abc1f883d7e782977",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 123,
+		songImg: "uploads/image00005.jpeg",
+		songSrc: "uploads/audio00025.mp3",
+		__v: 0,
+		create_at: "2021-05-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bf9234bc1f883d7e782978",
+		songName: "Don't Wanna Know",
+		artistName: "Maroon 5, Kendrick Lamar",
+		artistId: ["6162d9ba2fddb7174c15dc43"],
+		albumId: "61bf9234bc1f883d7e782978",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 124,
+		songImg: "uploads/image00006.jpeg",
+		songSrc: "uploads/audio00026.mp3",
+		__v: 0,
+		create_at: "2021-08-20T07:22:32.041Z",
+	},
+	{
+		_id: "61bf9234bc1f883d7e782978",
+		songName: "Don't Wanna Know",
+		artistName: "Maroon 5, Kendrick Lamar",
+		artistId: ["6162d9ba2fddb7174c15dc43"],
+		albumId: "61bf9234bc1f883d7e782978",
+		genreId: "6162bbec9298eb65df91223c",
+		likeCount: 124,
+		songImg: "uploads/image00006.jpeg",
+		songSrc: "uploads/audio00026.mp3",
+		__v: 0,
+		create_at: "2021-08-20T07:22:32.041Z",
+	},
+];
+const title = "Collection";
+
 function Discovery() {
-    return ( <div>
-        Discovery Page
-    </div> );
+	return (
+		<div>
+			<ListView props={{ songs, title }} />
+		</div>
+	);
 }
 
 export default Discovery;
