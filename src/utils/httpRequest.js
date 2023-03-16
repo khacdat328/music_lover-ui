7e4dc9e40e81b265f9e59e4e213454d72ef33928
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-03-16 16:55:32 +0700

    fetch data from API

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -2,14 +2,12 @@ import axios from "axios";
 
 const request = axios.create({ baseURL: "http://localhost:4000" });
 export const get = async (url, option = {}) => {
-	await axios
-		.get(url, option)
-		.then((response) => {
-			return response.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	try {
+		const data = await request.get(url, option);
+		return data.data;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export default request;
